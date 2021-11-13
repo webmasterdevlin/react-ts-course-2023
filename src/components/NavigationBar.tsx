092d5fabc9d643ca6571e06075d4b42cc034d29e
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-13 14:30:09 +0100

    update

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,16 +1,31 @@
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { pathNames } from "../LazyRoutes";
+import * as S from "./NavigationBar.style";
 
 const NavigationBar = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   return (
     <div>
-      <button onClick={() => navigate(pathNames.work)}>WORK TODOS</button>
-      <button onClick={() => navigate(pathNames.shoppingList)}>
+      <S.NavButton
+        active={pathNames.work === pathname}
+        onClick={() => navigate(pathNames.work)}
+      >
+        WORK TODOS
+      </S.NavButton>
+      <S.NavButton
+        active={pathNames.shoppingList === pathname}
+        onClick={() => navigate(pathNames.shoppingList)}
+      >
         SHOPPING LIST
-      </button>
-      <button onClick={() => navigate(pathNames.auth)}>LOGIN</button>
+      </S.NavButton>
+      <S.NavButton
+        active={pathNames.auth === pathname}
+        onClick={() => navigate(pathNames.auth)}
+      >
+        LOGIN
+      </S.NavButton>
     </div>
   );
 };

7f69616e03606a7e47d59163bad3b4cb7755a08e
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-13 14:37:46 +0100

    update

---
+++ src/views/MainLayout.style.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: #fafafa;
+`;
+
+export const SecondaryWrapper = styled.div`
+  background-color: #fafafa;
+  padding: 1rem;
+`;
+
+export const Header = styled.div`
+  background-color: lightgray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+`;

5eaac14c51402bf67b23e484d7340f0aa1e6f5c7
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-13 10:56:19 +0100

    update

---
+++ src/CarClass.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Statement } from "typescript";
+
+interface IState {
+  name: string;
+}
+interface IProps {
+  food?: string;
+}
+
+export default class CarClass extends React.Component<IProps, IState> {
+  state = {
+    name: "Klementin",
+  };
+
+  async componentDidMount() {
+    // HTTP REQUESTS
+    await Promise.resolve(
+      this.setState({
+        name: "Apelsin",
+      })
+    );
+  }
+
+  render() {
+    const { food } = this.props;
+
+    return (
+      <h1
+        onClick={() =>
+          this.setState({
+            name: "Updated Klementin",
+          })
+        }
+      >
+        Hello Class based component: {this.state.name} {food}
+      </h1>
+    );
+  }
+}

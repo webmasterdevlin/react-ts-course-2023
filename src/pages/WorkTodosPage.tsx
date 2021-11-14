12eb4e9ed483eba42a4bb78694e3c37dc27dbfd0
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-14 11:15:55 +0100

    update

--- src/pages/WorkTodosPage.tsx
+++ src/pages/WorkTodosPage.tsx
@@ -28,22 +28,28 @@ const WorkTodosPage = () => {
 
   return (
     <MainLayout>
-      <h1>Work Todos Page Works! {todos.length}</h1>
-      {todos.map((t) => (
-        <S.IconWrapper key={t.id}>
-          <div>
-            {t.completed ? (
-              <S.IconWrapper>
-                <CheckIcon onClick={() => {}} />
-              </S.IconWrapper>
-            ) : (
-              <S.IconWrapper>
-                <CircleIcon onClick={() => {}} />
-              </S.IconWrapper>
-            )}
-          </div>
-        </S.IconWrapper>
-      ))}
+      <div style={{ margin: "6rem 0" }}>
+        <h1>Work Todos Page Works! {todos.length}</h1>
+        {todos.map((t) => (
+          <S.ItemWrapper key={t.id}>
+            <div>
+              {t.completed ? (
+                <S.IconWrapper>
+                  <CheckIcon onClick={() => {}} />
+                </S.IconWrapper>
+              ) : (
+                <S.IconWrapper>
+                  <CircleIcon onClick={() => {}} />
+                </S.IconWrapper>
+              )}
+            </div>
+            <S.ItemName done={t.completed} onClick={() => {}}>
+              {t.title}
+            </S.ItemName>
+          </S.ItemWrapper>
+        ))}
+      </div>
+      <h3>{loading ? "Loading..." : "You have " + todos.length + " todos"}</h3>
     </MainLayout>
   );
 };

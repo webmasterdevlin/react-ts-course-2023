98c5ecbb8a5ebd4ddbf8cd15ff057c500a4b36d7
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-14 16:23:17 +0100

    update

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -4,5 +4,7 @@ declare namespace Cypress {
   interface Chainable {
     getCommand(url: string, responseBody: Array<any>): void;
     deleteCommand(url: string): void;
+    postCommand(url: string, requestBody: any): void;
+    SetupInputFieldsCommand(): void;
   }
 }
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,6 +3,7 @@
 ///<reference types="cypress" />
 
 import "@testing-library/cypress/add-commands";
+import { v4 as uuidv4 } from "uuid";
 
 Cypress.Commands.add("getCommand", (url: string, responseBody: Array<any>) => {
   cy.intercept("GET", url, {
@@ -15,4 +16,18 @@ Cypress.Commands.add("deleteCommand", (url: string) => {
   cy.intercept("DELETE", url, {
     statusCode: 200,
   });
+});
+
+Cypress.Commands.add("postCommand", (url: string, requestBody: any) => {
+  requestBody.id = uuidv4();
+
+  cy.intercept("POST", url, {
+    statusCode: 201,
+    body: requestBody,
+  });
+});
+
+Cypress.Commands.add("SetupInputFieldsCommand", () => {
+  cy.get("[data-testid=todo-input]").as("Input");
+  cy.get("[data-testid=save-todo]").as("Save");
 });

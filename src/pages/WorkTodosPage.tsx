56ec7d3330c9301ab49a9bbd204c9abae5a5b9d6
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-14 10:28:27 +0100

    update

--- src/pages/WorkTodosPage.tsx
+++ src/pages/WorkTodosPage.tsx
@@ -1,15 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { EndPoints } from "../api/axiosConfig";
+import { getAxios } from "../api/genericApiCalls";
+import { Todo } from "../models/todoType";
 import MainLayout from "../views/MainLayout";
 
 const WorkTodosPage = () => {
-  
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-  
+    getTodosAsync();
   }, []);
+
+  const getTodosAsync = async () => {
+    setLoading(true);
+    try {
+      const { data } = await getAxios<Todo[]>(EndPoints.todos);
+      setTodos(data);
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
 
   return (
     <MainLayout>
-      <h1>Work Todos Page Works!</h1>
+      <h1>Work Todos Page Works! {todos.length}</h1>
     </MainLayout>
   );
 };

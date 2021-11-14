e2fc857d01455f2c751fb61d79e1b68731fd2182
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-14 15:51:52 +0100

    update

---
+++ src/components/FormSubmission.tsx
@@ -0,0 +1,46 @@
+import { ErrorMessage, Formik } from "formik";
+import * as yup from "yup";
+import * as S from "../pages/Pages.style";
+
+type Props = {
+  save: (values: any) => void;
+};
+
+const FormSubmission = ({ save }: Props) => {
+  return (
+    <Formik
+      initialValues={{ id: "", title: "", completed: false }}
+      validationSchema={yup.object({
+        title: yup.string().label("Title").min(2).required(),
+      })}
+      onSubmit={(values, actions) => {
+        save(values);
+        actions.resetForm();
+      }}
+    >
+      {(formikProps) => (
+        <section>
+          <ErrorMessage name="title" />
+          <S.Form>
+            <S.Field
+              label="title"
+              id="title"
+              type="text"
+              name="title"
+              data-testid="todo-input"
+            />
+            <S.Button
+              disabled={formikProps.values.title.length < 2}
+              type="submit"
+              data-testid="save-todo"
+            >
+              + ADD
+            </S.Button>
+          </S.Form>
+        </section>
+      )}
+    </Formik>
+  );
+};
+
+export default FormSubmission;

1c533437b101ca7e246e8195cac47a85c3e23d18
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-14 10:47:50 +0100

    update

--- src/pages/WorkTodosPage.tsx
+++ src/pages/WorkTodosPage.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { Check as CheckIcon, Circle as CircleIcon } from "react-feather";
+
 import { EndPoints } from "../api/axiosConfig";
 import { getAxios } from "../api/genericApiCalls";
 import { Todo } from "../models/todoType";
 import MainLayout from "../views/MainLayout";
+import * as S from "./Pages.style";
 
 const WorkTodosPage = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -26,6 +29,11 @@ const WorkTodosPage = () => {
   return (
     <MainLayout>
       <h1>Work Todos Page Works! {todos.length}</h1>
+      {todos.map((t) => (
+        <S.IconWrapper key={t.id}>
+          <div>{t.title}</div>
+        </S.IconWrapper>
+      ))}
     </MainLayout>
   );
 };

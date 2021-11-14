2f5cb95be1d3dd1a0eec64612f6396d10bcf9838
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-14 09:48:30 +0100

    update

--- src/pages/WorkTodosPage.tsx
+++ src/pages/WorkTodosPage.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import MainLayout from "../views/MainLayout";
 
 const WorkTodosPage = () => {
+  useEffect(() => {
+    fetch("/api/todos");
+  }, []);
+
   return (
     <MainLayout>
       <h1>Work Todos Page Works!</h1>
